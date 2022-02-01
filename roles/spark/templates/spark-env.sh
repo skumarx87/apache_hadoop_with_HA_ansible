d25735f34caccd3487a363c306dfa5a1842cc689
Linuxuser [Admin (at) HostAdvice] <hadoop@laksha.tanu.com>
2022-02-02 04:20:39 +0530

    initial commit

---
+++ roles/spark/templates/spark-env.sh
@@ -0,0 +1,7 @@
+SPARK_WORKER_CORES=12
+HADOOP_CONF_DIR={{ BIGDATA_DIR }}/Envs/hadoop/etc/hadoop
+SPARK_PID_DIR={{ BIGDATA_DIR }}/pids
+SPARK_LOG_DIR={{ BIGDATA_DIR }}/logs
+
+source ~/.bash_profile
+export SPARK_DIST_CLASSPATH=$(hadoop classpath)
