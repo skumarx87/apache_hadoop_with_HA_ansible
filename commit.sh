d33e58e8e4e86df1caa1cc55f6d3afb81519637b
skumarx87 <hadoop@sparkmaster.example.com>
2024-02-04 10:17:31 +0000

    initial commit

--- commit.sh
+++ commit.sh
@@ -1,3 +1,5 @@
+cd /home/hadoop/apache_hadoop_with_HA_ansible
+
 git add *.yml
 git add roles/*
 git add vars/*
@@ -5,5 +7,6 @@ git add *.sh
 git add commit.sh
 
 git commit -am "initial commit"
+git remote add origin https://skumarx87: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@github.com/skumarx87/apache_hadoop_with_HA_ansible.git
 
 git push origin
