0cf30feae2196fd6ce6cde0018e6394bcc36969b
skumarx87 <hadoop@sparkmaster.example.com>
2024-02-04 10:11:01 +0000

    initial commit

--- roles/scripts/templates/bigadm.py
+++ roles/scripts/templates/bigadm.py
@@ -5,6 +5,7 @@ import sys
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 import subprocess
+import re
 
 class bigadm:
     def __init__(self):
@@ -36,7 +37,7 @@ class bigadm:
         try:
             process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             stdout, stderr = process.communicate(timeout=5)
-            self.logger.info(stdout.strip())
+            #self.logger.info(stdout.strip())
             if process.returncode == 0:
                 return stdout
             else:
@@ -89,35 +90,63 @@ class bigadm:
         parser.add_argument('--statusAll',help='for starting service')
         parser.add_argument('--status',help='for starting service')
         return parser.parse_args()
+
+    def check_string_pattern(self,string,keyword):
+        if string is None:
+            return False
+        else:
+            match=re.search(keyword,string)
+            if match:
+                return True
+            else:
+                return False
     
     def check_process_of_pid_over_ssh(self,host,pid_file,process_name):
         cmd = ['ssh',host,' ','cat',pid_file]
         pid=self.run_command_over_ssh(cmd)
         if pid is not None:
             cmd = ['ssh',host,'ps','-p',pid]
-            self.run_command_over_ssh(cmd)
+            res=self.run_command_over_ssh(cmd)
+            is_running = self.check_string_pattern(res,process_name)
+            return is_running
+
 
 
 
     def hdfs_service(self,action):
         if action == 'status':
-            nn_pid_file = "{}/pids/hadoop-hadoop-namenode.pid".format(self.env_bigdata_root_value)
+            nm_pid_file = "{}/pids/hadoop-hadoop-namenode.pid".format(self.env_bigdata_root_value)
             dn_pid_file = "{}/pids/hadoop-hadoop-datanode.pid".format(self.env_bigdata_root_value)
             jn_pid_file = "{}/pids/hadoop-hadoop-journalnode.pid".format(self.env_bigdata_root_value)
             nm_hosts = self.parse_ansible_inventory('nameNode')
             dn_hosts = self.parse_ansible_inventory('dataNode')
             jn_hosts = self.parse_ansible_inventory('journalNode')
-            print(nm_hosts)
-            print(dn_hosts)
-            print(jn_hosts)
+            #print(nm_hosts)
+            #print(dn_hosts)
+            #print(jn_hosts)
 
-            nn_pid = self.read_pid_from_file(nn_pid_file)
+            #nn_pid = self.read_pid_from_file(nn_pid_file)
             for host in jn_hosts:
-                print(host)
-                self.check_process_of_pid_over_ssh(host,jn_pid_file,'java')
+                is_running = self.check_process_of_pid_over_ssh(host,jn_pid_file,'java')
+                if is_running:
+                    self.logger.info("Journal Node {} Running...".format(host))
+                else:
+                    self.logger.info("Journal Node {} is not Running...".format(host))
+
             for host in dn_hosts:
-                print(host)
-                self.check_process_of_pid_over_ssh(host,dn_pid_file,'java')
+                is_running=self.check_process_of_pid_over_ssh(host,dn_pid_file,'java')
+                if is_running:
+                    self.logger.info("Data Node {} Running...".format(host))
+                else:
+                    self.logger.info("Data Node {} is not Running...".format(host))
+            for host in nm_hosts:
+                is_running=self.check_process_of_pid_over_ssh(host,nm_pid_file,'java')
+                if is_running:
+                    self.logger.info("Name Node {} Running...".format(host))
+                else:
+                    self.logger.info("Name Node {} is not Running...".format(host))
+                    
+                
             '''
                 cmd = ['ssh',host,' ','cat',jn_pid_file]
                 pid=self.run_command_over_ssh(cmd)
@@ -125,11 +154,6 @@ class bigadm:
                     cmd = ['ssh',host,'ps','-p',pid]
                     self.run_command_over_ssh(cmd)
             '''
-            self.logger.info("name node pid : {}".format(nn_pid))
-            if self.is_check_pid_running(nn_pid):
-                self.logger.info("name node pid is running")
-            else:
-                self.logger.error("name node pid is not  running")
 
 
 
